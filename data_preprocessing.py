ee000de8661262eccc517a13397ab97a7a74a07b
vihaniscool <75142511+vihaniscool@users.noreply.github.com>
2023-11-12 03:41:44 +1200

    Add files via upload

---
+++ data_preprocessing.py
@@ -0,0 +1,65 @@
+#Text Data Preprocessing Lib
+import nltk
+nltk.download('punkt')
+
+from nltk.stem import PorterStemmer
+stemmer = PorterStemmer()
+
+import json
+import pickle
+import numpy as np
+
+words=[]
+classes = []
+word_tags_list = []
+ignore_words = ['?', '!',',','.', "'s", "'m"]
+train_data_file = open('C:/Users/VihanPC/Downloads/PRO-C119-Student-Boilerplate-Code-main/PRO-C119-Student-Boilerplate-Code-main/intents.json').read()
+intents = json.loads(train_data_file)
+
+# function for appending stem words
+def get_stem_words(words, ignore_words):
+    stem_words = []
+    for word in words:
+        if word not in ignore_words:
+            w = stemmer.stem(word.lower())
+            stem_words.append(w)  
+    return stem_words
+
+for intent in intents['intents']:
+    
+        # Add all words of patterns to list
+        for pattern in intent['patterns']:            
+            pattern_word = nltk.word_tokenize(pattern)            
+            words.extend(pattern_word)                      
+            word_tags_list.append((pattern_word, intent['tag']))
+        # Add all tags to the classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+            stem_words = get_stem_words(words, ignore_words)
+
+print(stem_words)
+print(word_tags_list[0]) 
+print(classes)   
+
+#Create word corpus for chatbot
+def create_bot_corpus(stem_words, classes):
+
+    stem_words = sorted(list(set(stem_words)))
+    classes = sorted(list(set(classes)))
+
+    pickle.dump(stem_words, open('C:/Users/VihanPC/Downloads/PRO-C119-Student-Boilerplate-Code-main/PRO-C119-Student-Boilerplate-Code-main/words.pkl','wb'))
+    pickle.dump(classes, open('C:/Users/VihanPC/Downloads/PRO-C119-Student-Boilerplate-Code-main/PRO-C119-Student-Boilerplate-Code-main/classes.pkl','wb'))
+
+    return stem_words, classes
+
+stem_words, classes = create_bot_corpus(stem_words,classes)  
+
+print(stem_words)
+print(classes)
+
+#Create Bag Of Words
+training_data = []
+numot = len(classes)
+lables = [0]*numot
+print(lables)
+#Create training data
